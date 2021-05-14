b5974c1d58ad7aec9e29f8da12b7439abab84edc
Syahnur Nizam <syahnurnizam197@gmail.com>
2021-05-14 18:12:26 +0800

    add dotenv

--- store/index.js
+++ store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "../reducers/modal-slice";
+import { REDUX_DEVTOOL_ENABLE } from "../utils/config";
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
   },
-  devTools: true,
+  devTools: REDUX_DEVTOOL_ENABLE,
 });
